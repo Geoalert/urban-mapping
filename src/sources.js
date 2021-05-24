f4c4cf102fb86ee5af9df1906da52cfae84503de
dqunbp <dqunbp@gmail.com>
2021-05-24 12:13:58 +0300

    update tiles url

--- src/sources.js
+++ src/sources.js
@@ -1,14 +1,12 @@
 export function loadGTPointsSource(map) {
   map.addSource("points-gt", {
     type: "vector",
-    url:
-      "https://demo.geoalert.io/data/GT_cluster_densest_as_needed_max25K_features_max100Kbytes.json"
+    url: "https://data.geoalert.io/data/GT_cluster_densest_as_needed_max25K_features_max100Kbytes.json",
   });
 }
 export function loadOSMPointsSource(map) {
   map.addSource("points-osm", {
     type: "vector",
-    url:
-      "https://demo.geoalert.io/data/OSM_cluster_densest_as_needed_max25K_features_max100Kbytes.json"
+    url: "https://data.geoalert.io/data/OSM_cluster_densest_as_needed_max25K_features_max100Kbytes.json",
   });
 }
